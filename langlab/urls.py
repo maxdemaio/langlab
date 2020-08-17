f1046514b75f7c9107e11191a3df1757e05e84c7
Maxwell DeMaio <maxdman321@gmail.com>
2020-08-17 19:25:59 -0400

    Changed admin url to prevent automated attacks

--- langlab/urls.py
+++ langlab/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('maxadmin/', admin.site.urls),
     path("", include("hello.urls")),
     path("conjugate/", include("conjugate.urls"))
 ]
